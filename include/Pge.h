040fdef07461ef52b10a047af2dd71dd2e34fcfb
David <dquispellocallasi@gmail.com>
2026-05-03 20:46:09 -0500

    "primeros funciones basicas"

---
+++ include/Pge.h
@@ -0,0 +1,18 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+#include <iostream>
+
+const int PAGE_SIZE = 4096;
+
+struct Page{
+    int pade_id;
+    int next_page;
+    int data_count;
+
+    
+
+
+};
+
+#endif
